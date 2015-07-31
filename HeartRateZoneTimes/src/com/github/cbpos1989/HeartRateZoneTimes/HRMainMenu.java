16d40652a5f6eb8a82bcd47821ddaad5cd499c5d
Colm O'Sullivan <cbpos1989@gmail.com>
2015-07-31 21:18:53 +0100

    Created new dev branch to work on a new updated version of the HeartRateApp

---
+++ HeartRateZoneTimes/src/com/github/cbpos1989/HeartRateZoneTimes/HRMainMenu.java
@@ -0,0 +1,66 @@
+package com.github.cbpos1989.HeartRateZoneTimes;
+
+
+import java.awt.Color;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.ScrollPaneConstants;
+import javax.swing.SwingConstants;
+import javax.swing.JTextPane;
+import javax.swing.JScrollPane;
+import javax.swing.JButton;
+import java.awt.Insets;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class HRMainMenu extends JFrame {
+
+	private JPanel contentPane;
+	private final JScrollPane scrollPane;
+
+	/**
+	 * Create the frame.
+	 */
+	public HRMainMenu() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 651, 525);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(null);
+		setContentPane(contentPane);
+		
+		JLabel lblHeartRateApp = new JLabel("Heart Rate App Main Menu");
+		lblHeartRateApp.setFont(new Font("Tunga", Font.BOLD, 18));
+		lblHeartRateApp.setHorizontalAlignment(SwingConstants.CENTER);
+		lblHeartRateApp.setBounds(114, 11, 196, 32);
+		contentPane.add(lblHeartRateApp);
+		
+		final JTextPane textPane = new JTextPane();
+		textPane.setBackground(Color.LIGHT_GRAY);
+		textPane.setBounds(10, 11, 402, 240);
+		textPane.setEditable(false);
+	
+		scrollPane = new JScrollPane(textPane);
+		scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+		scrollPane.setBounds(10, 50, 450, 425);
+		contentPane.add(scrollPane);
+		
+		JButton btnAddNewEntry = new JButton("Add New Entry");
+		btnAddNewEntry.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				//TODO Create new GUI to allow users to enter heart rate times.
+			}
+		});
+		btnAddNewEntry.setMargin(new Insets(2, 4, 2, 4));
+		btnAddNewEntry.setFont(new Font("Calibri", Font.BOLD, 14));
+		btnAddNewEntry.setBounds(480, 61, 115, 35);
+		contentPane.add(btnAddNewEntry);
+		
+		
+	
+	}
+}
--- HeartRateZoneTimes/src/com/github/cbpos1989/HeartRateZoneTimes/HRMenu.java
+++ HeartRateZoneTimes/src/com/github/cbpos1989/HeartRateZoneTimes/HRMenu.java
@@ -59,7 +59,7 @@ public class HRMenu extends JFrame  {
 		menuEnabled = true;
 		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 382, 452);
+		setBounds(100, 100, 190, 293);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		//contentPane.setLayout(new BorderLayout(0, 0));
